3c8ffcff5ad6eec63ac19fa9c8966a53cc18b2e4
LENOVO L380 YOGA <restualfazhuri@gmail.com>
2024-11-04 15:26:19 +0700

    first init github

---
+++ 03-CastingTipeData/main.py
@@ -0,0 +1,37 @@
+# Casting
+# mengubah tipe data ke tipe data lain
+# tipe data = int, float, str, bool
+
+## interger
+print('===INTEGER===') 
+data_int = 9
+print ('data : ', data_int, "tipe = ", type (data_int))
+
+# ubah integer ke float
+data_float = float(data_int)
+# ubah integral ke string
+data_str = str(data_int)
+# ubah integer ke boolean
+data_bool = bool(data_int)
+# cetak
+print('data : ', data_float, 'tipe = ', type (data_float))
+print('data : ', data_str, 'tipe = ', type (data_str))
+print('data : ', data_bool, 'tipe = ', type (data_bool))
+
+## Float 
+print('===Float===')
+data_float = 3.14
+print('data : ', data_float, 'tipe = ', type (data_float))
+
+# int
+data_int = int(data_float)
+# bool
+data_bool = bool(data_float)
+# str 
+data_str = str(data_float)
+
+#print
+
+print('data : ', data_int, 'tipe = ', type (data_int))
+print('data : ', data_bool, 'tipe = ', type (data_bool))
+print('data : ', data_str, 'tipe = ', type (data_str))
